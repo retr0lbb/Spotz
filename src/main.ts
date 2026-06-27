4769ac7ad27a84820574575007a5aee85af10c26
retr0lbb <retr0lbb@gmail.com>
2026-06-27 03:22:02 -0300

    feat: update cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true, // aceita qualquer origem
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
